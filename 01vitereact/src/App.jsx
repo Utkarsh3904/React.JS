42962196ac8a580b50eb11de4746ce3406e1b5d8
Utkarsh Srivastava <millenniumatom@gmail.com>
2025-04-02 01:39:35 +0530

    edited as Chai function is injected

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
-
+import Chai from './Chai.jsx'
 
 function App() {
  
-
   return (
+    <>
+    <Chai />
    <h1>chai with react with vite</h1>
+   </>
   )
 }
-
 export default App
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h1>we r gonna build chai </h1>
+    )
+}
+
+export default Chai
