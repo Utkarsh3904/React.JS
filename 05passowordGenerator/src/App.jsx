e4ef95924490f095a246aa2f4c38ee988397a113
Utkarsh Srivastava <millenniumatom@gmail.com>
2025-04-08 01:05:36 +0530

    Password Generator

--- 05passowordGenerator/src/App.jsx
+++ 05passowordGenerator/src/App.jsx
@@ -8,12 +8,12 @@ import { useState,useCallback, useEffect ,useRef} from 'react'  //here useCallba
 import './App.css'
 
 function App() {
-  const [length,setlength] = useState(0)
+  const [length,setlength] = useState(6)  //phle 0 tha tho refresh par sirf ek alphabet show ho rha tha ab, 6 krne par refreh par bhi 6 ho rhe hai
   const [numberAllowed,setnumberAllowed] = useState(false)
   const [charAllowed,setcharAllowed] = useState(false);
   const [password,setpassword] = useState("");  //isme password ki value store hogi, phle isme bhi valse likha tha lekin vo fir container me dikh rha tha likha hua
 
-  //useRef hook
+  //useRef HOOK
   const passwordref =useRef(null)
 
   // function to generate a password HOOK
@@ -34,19 +34,22 @@ function App() {
 
   },[length, numberAllowed, charAllowed]) //before useCallback the brackets[] we have to create a function
 
-  //for COPY
+  //for COPY ,HOOK
 const CopyToClipboard = useCallback(() => {
   passwordref.current?.select() // ye select karega password ko, ? is for optional select may be current value zero ho
   passwordref.current?.setSelectionRange(0, 3); // might be somewhere useful its for range
   window.navigator.clipboard.writeText(password)
 },[password])
 
+//HOOK
   useEffect(() => {
     passwordGenerator()
   } ,[length, numberAllowed,charAllowed, passwordGenerator]) // ye useEffect hai jo function ko call karega jab bhi koi change hoga
 
+
   return (
     <>
+    {/* Grey n White Container */}
     <div className = "w-full max-w-md  mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-600 ">
        <h1 className ="text-white text-center ">Password Generator</h1>
       <div className = "flex shadow rounded-lg overflow-hidden mb-2">  
@@ -57,12 +60,21 @@ const CopyToClipboard = useCallback(() => {
       placeholder = "password dedo"                              // isme jo likhenge vo white box me aayega
       readOnly
       ref = {passwordref}                                       // ye password ki value ko copy karne ke liye hai
-      
       />
+
+      {/* Button */}
       <button onClick = {CopyToClipboard}
-       className = "outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">copy</button>
+    
+       className = {`outline-none bg-blue-700  transition-all duration-200 text-white px-3 py-0.5 shrink-0 
+             rounded-r cursor-pointer hover:scale-105 active:scale-95`}>
+              {/* ye jo svg hai vo icon hai */}
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
+             </svg>
+              </button>
     </div> 
     
+  {/* Cursor-pointer for length */}
   <div className = "flex text-sm gap-x-2">
     <div className = "flex items-center gap-x-1"></div>
     <input 
@@ -75,6 +87,8 @@ const CopyToClipboard = useCallback(() => {
     />
     <label> Length:{length}</label>
   </div>
+
+  {/* Number checkbox */}
   <div className = "flex items-center gap-x-1">
     <input 
     type="checkbox"
@@ -84,16 +98,18 @@ const CopyToClipboard = useCallback(() => {
     onChange= {()=> {setnumberAllowed((prev) => !prev);}}
     />
     <label htmlFor="numberInput"> Numbers</label>
-</div>
-<div className = "flex items-center gap-x-1">
-  <input 
-  type="checkbox"
-  defaultChecked={charAllowed}
-  id="characterInput"
-  onChange={()=> {setcharAllowed((prev) => !prev);}}  //previous value true false hoti rhegi
-  />
-  <label htmlFor="characterInput">Characters</label>
- </div>
+  </div>
+
+ {/* Character checkbox */}
+ <div className = "flex items-center gap-x-1">
+   <input 
+   type="checkbox"
+   defaultChecked={charAllowed}
+   id="characterInput"
+   onChange={()=> {setcharAllowed((prev) => !prev);}}  //previous value true false hoti rhegi
+   />
+   <label htmlFor="characterInput">Characters</label>
+  </div>
 </div>
 
  </>
