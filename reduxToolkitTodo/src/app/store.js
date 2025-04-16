25716627469029d34c3361dab8e56d3b1f8878d0
Utkarsh Srivastava <millenniumatom@gmail.com>
2025-04-17 00:47:28 +0530

    redux first half of reducers

---
+++ reduxToolkitTodo/src/app/store.js
@@ -0,0 +1,7 @@
+import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from '../features/todo/todoSlice'; 
+//yr upar vala isliye likha hai ki store ko knowledge honi chaiye reducers ki ye bhi kuch hai   
+
+export const store = configureStore({  //isme key vlaue daalte hai bus
+    reducer: todoReducer
+})
